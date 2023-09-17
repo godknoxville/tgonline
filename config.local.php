10c80668dbf6ae6c50c1edc0349199b2b04874fd
godknoxville <145266869+godknoxville@users.noreply.github.com>
2023-09-17 09:36:55 -0300

    ot

---
+++ config.local.php
@@ -0,0 +1,15 @@
+<?php
+// place for your configuration directives, so you can later easily update myaac
+$config['installed'] = true;
+$config['env'] = 'prod'; // dev or prod
+$config['mail_enabled'] = true;
+$config['server_path'] = 'C:/otserv_860/';
+$config['mail_admin'] = 'admin@gmail.com';
+$config['mail_address'] = 'admin@gmail.com';
+$config['date_timezone'] = 'America/Sao_Paulo';
+$config['client'] = '860';
+$config['anonymous_usage_statistics'] = true;
+$config['session_prefix'] = 'myaac_p391g8qk_';
+$config['cache_prefix'] = 'myaac_5qtc42yj_';
+
+$config['highscores_ids_hidden'] = array(2, 3, 4, 5);
